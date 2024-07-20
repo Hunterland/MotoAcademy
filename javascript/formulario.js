14881ef43c30ffb5ad381d51d572079ec0eb5925
Alan Barroncas <alanbarroncas@gmail.com>
2024-07-19 20:30:07 -0400

    subindo aula de formulario em js

---
+++ javascript/formulario.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const formulario = document.getElementById("form");
+  const resultado = document.getElementById("result");
+
+  formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const nome = document.getElementById("nome");
+    const email = document.getElementById("email");
+    const mensagem = document.getElementById("mensagem");
+
+    resultado.innerHTML = `
+    
+        <h2>Dados Enviados</h2>
+        <p><strong>Nome: </strong>${nome}</p>
+        <p><strong>Email: </strong>${email}</p>
+        <p><strong>Mensagem: </strong>${mensagem}</p>
+    `;
+
+    formulario.reset();
+  });
+});
