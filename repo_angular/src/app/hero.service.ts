4c34e4f822e40d67e1b3096b09b2b58c652c12d7
Alan Barroncas <alanbarroncas@gmail.com>
2024-09-30 19:14:31 -0400

    atualizando repo

--- repo_angular/src/app/hero.service.ts
+++ repo_angular/src/app/hero.service.ts
@@ -1,24 +1,49 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+
+  constructor(
+    private messageService: MessageService,
+    private httpClient: HttpClient
+  ) {}
+
+  heroesUrl = 'api/heroes'
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: listagem herois');
-    return heroes;
+    return this.httpClient.get<Hero[]>(this.heroesUrl).pipe(
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
   }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find(hero => hero.id == id)!;
-    this.messageService.add(`HeroService: heroi selecionado id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`
+    return this.httpClient.get<Hero>(url).pipe(
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
   }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.httpClient.put(this.heroesUrl, hero).pipe(
+      catchError(this.handleError<any>('updateHero'))
+    )
+  }
+
+
+
+
 }
